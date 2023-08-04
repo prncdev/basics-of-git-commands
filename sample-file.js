937b28fe7ad20223de2194b0bc89e5995eb9e19e
Prince Chaudhary <princedavi94@gmail.com>
2023-08-05 05:20:43 +0530

    Initial commit

---
+++ sample-file.js
@@ -0,0 +1,14 @@
+function FindTwSum(numbers, target) {
+  // We will be using hash table Data Structure to solve this problem.
+  const numbersTable = {};
+  for(let i = 0; i < numbers.length; i++) {
+    const currnetNumber = numbersTable[numbers[i]];
+    if(currnetNumber >= 0) return [currnetNumber, i];
+    else {
+      const numberToFind = target - numbers[i];
+      numbersTable[numberToFind] = i;
+    }
+  }
+
+  return null;
+}
